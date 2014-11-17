98870985f05326eab61ea39d39875f79c4bcc7b0
Robert <rsmith8917@gmail.com>
2014-11-17 18:05:37 -0500

    Separated WPFInterface from CalculatorModel.  Created ConsoleInterface.

---
+++ CalculatorConsoleInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calculator = new Calc();
+            Console.WriteLine(calculator.EvaluateReversePolish("3 SQRT").ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CalculatorModel/CalculatorUnitTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator
+{
+    [TestClass]
+    public class AdditionOperatorUnitTests
+    {
+        [TestMethod]
+        public void ReversePolishThreeFourPlusEqualsSevenTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("3 4 +");
+            Assert.AreEqual(result, 7);
+        }
+
+        [TestMethod]
+        public void ReversePolishThreeFourMinusEqualsNegativeOneTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("3 4 -");
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void ReversePolishThreeFourTimesEqualsTwelveTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("3 4 *");
+            Assert.AreEqual(result, 12);
+        }
+
+        [TestMethod]
+        public void ReversePolishThreeFourDivideEqualsThreeFourthsTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("3 4 /");
+            Assert.AreEqual(result, 0.75);
+        }
+
+        [TestMethod]
+        public void ReversePolishOneFortyFourSquareRootEqualsTwelveTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("144 sqrt");
+            Assert.AreEqual(result, 12);
+        }
+
+        [TestMethod]
+        public void ReversePolishNineSquareRootEqualsThreeTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("9 SQRT");
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void ReversePolishMultipleOperationsTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("5 1 2 + 4 * + 3 -");
+            Assert.AreEqual(result, 14);
+        }
+
+        [TestMethod]
+        public void ReversePolishFractionalOperationsTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("5.2 1 2.3 + 4 * + 3.7 -");
+            Assert.AreEqual(result, 14.7);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ReversePolishBadTokenThrowsExceptionTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("q 5 2 A");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ReversePolishEmptyExpressionThrowsExceptionTestMethod()
+        {
+            var calculator = new Calc();
+            var result = calculator.EvaluateReversePolish("");
+        }
+    }
+}
+
